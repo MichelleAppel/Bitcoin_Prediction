05dd57fe3ba60bc791091c2bd64093fe48885da4
Michelle <appel.michelle@gmail.com>
2017-05-16 12:57:45 +0200

    Normalise and denormalise results

--- APIs/LSTM.py
+++ APIs/LSTM.py
@@ -3,52 +3,49 @@ from keras.layers.core import Dense, Activation, Dropout
 from keras.layers.recurrent import LSTM
 from keras.models import Sequential
 import lstm_helper, time
-
 import numpy as np
-import random
-
 from Blockchain_test import return_data
 
 # Load data from Blockchain_test.py
 matrix = return_data()
 matrix = matrix[0] # Bitcoin price only
 
-# N_FEATURES = len(matrix)
+NORMALISATION = True
+DENORMALISATION = True
 
-BATCH_START = 0
-BATCH_SIZE = 30
-TIME_STEPS = 5
-
-INPUT_SIZE = len(matrix)-1 # Amount of features
-OUTPUT_SIZE = 1 # Bitcoin price
-
-TRAIN_TEST_RATIO = 0.7
-CELL_SIZE = 10
-LR = 10
+BATCH_SIZE = 256
+SEQ_LEN = 10
+PREDICTION_LEN = 4
 EPOCHS = 1
 
-train_X, train_y, test_X, test_y = lstm_helper.load_data(matrix, TIME_STEPS, False)
-
-N_BATCHES = int(np.size(train_X)/TIME_STEPS)
+INPUT_DIM = len(matrix) # Amount of features
+# N_FEATURES = len(matrix)
 N_FEATURES = 1
+
+OUTPUT_SIZE = 1 # Bitcoin price
+
+train_X, train_y, test_X, test_y, i_list = lstm_helper.load_data(matrix, SEQ_LEN, NORMALISATION)
+
+N_BATCHES = int(np.size(train_X) / SEQ_LEN)
+
 
 # Build model
 model = Sequential()
 
 model.add(LSTM(
     input_dim = 1,
-    output_dim = TIME_STEPS,
+    output_dim = SEQ_LEN,
     return_sequences = True))
 model.add(Dropout(0.2))
 
 model.add(LSTM(
-    100,
+    units = BATCH_SIZE,
     return_sequences = False,
     ))
 model.add(Dropout(0.2))
 
 model.add(Dense(
-    output_dim = 1
+    output_dim = 1,
     ))
 model.add(Activation('linear'))
 
@@ -57,20 +54,26 @@ model.compile(loss = 'mse', optimizer = 'rmsprop')
 
 print('compilation time : ', time.time() - start)
 
-print(train_X.reshape((N_FEATURES, N_BATCHES, TIME_STEPS)))
-
 # Train the model
 model.fit(
     train_X,
     train_y,
-    batch_size = 512,
+    batch_size = BATCH_SIZE,
     epochs = EPOCHS,
     validation_split = 0.05
 )
 
-print(test_X)
-
 # Plot
-predictions = lstm_helper.predict_sequences_multiple(model, test_X, 50, TIME_STEPS)
-print(predictions)
-lstm_helper.plot_results_multiple(predictions, test_y, TIME_STEPS)
+# Denormalised
+predictions = lstm_helper.predict_sequences_multiple(model, test_X, SEQ_LEN, PREDICTION_LEN, DENORMALISATION, i_list)
+print("test", lstm_helper.denormalise_windows(train_X, i_list)[0])
+print("pred", predictions[0])
+print("real", lstm_helper.denormalise_windows(test_y.reshape(len(test_y),1), i_list)[0:PREDICTION_LEN])
+lstm_helper.plot_results_multiple(predictions, test_y, PREDICTION_LEN, DENORMALISATION, i_list)
+
+# Normalised
+predictions = lstm_helper.predict_sequences_multiple(model, test_X, SEQ_LEN, PREDICTION_LEN, False, i_list)
+print("test_norm", train_X[0])
+print("pred_norm", predictions[0])
+print("real_norm", test_y[0:PREDICTION_LEN])
+lstm_helper.plot_results_multiple(predictions, test_y, PREDICTION_LEN, False, i_list)
--- APIs/lstm_helper.py
+++ APIs/lstm_helper.py
@@ -9,8 +9,12 @@ import matplotlib.pyplot as plt
 
 warnings.filterwarnings("ignore")
 
+def plot_results_multiple(predicted_data, true_data, prediction_len, denormalise, i_list):
+    true_data = true_data.reshape(len(true_data),1)
 
-def plot_results_multiple(predicted_data, true_data, prediction_len):
+    if denormalise:
+        true_data = denormalise_windows(true_data, i_list)
+
     fig = plt.figure(facecolor='white')
     ax = fig.add_subplot(111)
     ax.plot(true_data, label='True Data')
@@ -32,12 +36,15 @@ def load_data(matrix, seq_len, normalise_window):
     for index in range(len(matrix) - sequence_length):
         result.append(matrix[index: index + sequence_length])
 
+    i_list = []
     if normalise_window:
-        result = normalise_windows(result)
+        result, i_list = normalise_windows(result)
 
     result = np.array(result)
 
     row = round(0.9 * result.shape[0])
+    i_list.append(row)
+
     train = result[:int(row), :]
     np.random.shuffle(train)
     x_train = train[:, :-1]
@@ -48,19 +55,36 @@ def load_data(matrix, seq_len, normalise_window):
     x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
     x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
 
-    return [x_train, y_train, x_test, y_test]
+    return [x_train, y_train, x_test, y_test, i_list]
 
 
 def normalise_windows(window_data):
     normalised_data = []
+    i_list = [] # contains the initial values
+
     for window in window_data:
+        i_list.append(window[0])
         if not window[0] == 0:
             normalised_window = [((float(p) / float(window[0])) - 1) for p in window]
         else:
-            normalised_window = [(float(p)) for p in window]
+            normalised_window = [(float(p) - 1) for p in window]
         normalised_data.append(normalised_window)
-    return normalised_data
+    return normalised_data, i_list
 
+def denormalise_windows(window_data, i_list):
+    row = i_list[-1]
+    i_list = i_list[row:-1]
+
+    denormalised_data = []
+
+    for window, initial_val in zip(window_data, i_list):
+        if not initial_val == 0:
+            denormalised_window = [(float(initial_val)*(float(p) + 1)) for p in window]
+        else:
+            denormalised_window = [(float(p)) for p in window]
+        denormalised_data.append(denormalised_window)
+
+    return denormalised_data
 
 def build_model(layers):
     model = Sequential()
@@ -105,16 +129,20 @@ def predict_sequence_full(model, data, window_size):
     return predicted
 
 
-def predict_sequences_multiple(model, data, window_size, prediction_len):
-    # Predict sequence of 50 steps before shifting prediction run forward by 50 steps
+def predict_sequences_multiple(model, data, window_size, prediction_len, denormalise, i_list):
+    # Predict sequence of window_size steps before shifting prediction run forward by prediction_len steps
 
     prediction_seqs = []
-    for i in range(int(len(data) / prediction_len)):
+    for i in range(int(len(data) / prediction_len)-1):
         curr_frame = data[i * prediction_len]
         predicted = []
         for j in range(prediction_len):
             predicted.append(model.predict(curr_frame[newaxis, :, :])[0, 0])
             curr_frame = curr_frame[1:]
-            # curr_frame = np.insert(curr_frame, [window_size - 1], predicted[-1], axis=0)
+            curr_frame = np.insert(curr_frame, [window_size - 1], predicted[-1], axis=0)
         prediction_seqs.append(predicted)
+
+    if denormalise:
+        prediction_seqs = denormalise_windows(prediction_seqs, i_list)
+
     return prediction_seqs
